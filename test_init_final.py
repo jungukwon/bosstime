8d62e4008685ef450caa2f3895bd980031068cb0
jungukwon <60176957+jungukwon@users.noreply.github.com>
2020-03-16 23:53:38 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1311,6 +1311,10 @@ while True:
 				await PlaySound(voice_client1, './sound/뮨.mp3')
 			if message.content == '!냠냠' :
 				await PlaySound(voice_client1, './sound/냠냠.mp3')
+			if message.content == '!왓' :
+				await PlaySound(voice_client1, './sound/왓.mp3')
+			if message.content == '!왓1' :
+				await PlaySound(voice_client1, './sound/왓1.mp3')
 			if message.content == '!TJ' or message.content == '!tj' :
 				resultTJ = random.randrange(1,9)
 				await PlaySound(voice_client1, './sound/TJ' + str(resultTJ) +'.mp3')
